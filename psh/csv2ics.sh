e87e0ef2db9083523ef97da8d5b5b0926cc7526e
Simon Elbaz <selbaz@linagora.com>
2014-05-20 13:47:58 +0200

    ajout du shell csv2ics.sh

--- psh/csv2ics.sh
+++ psh/csv2ics.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-iconv -f UTF-16 -t UTF-8 profil_agglolo_1_withrecurrences.csv > profil_agglolo_1_withrecurrences.csv.iconv
+# iconv -f UTF-16 -t UTF-8 profil_agglolo_1_withrecurrences.csv > profil_agglolo_1_withrecurrences.csv.iconv
+
+cd ../output
+
+for i in `ls`
+do
+	iconv -f UTF-16 -t UTF-8 $i > $i.iconv
+done
+
+python csv2ics.py
+
